5b18ac05d29b368feeb5070f4daf98c16fcbaaf4
maximiliano.mastrocola <maximiliano.mastrocola@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-11-01 02:40:35 +0000

    Arbol de Categorias y Zonas funcionando

--- Centur/DataAccessLayer/BuscarServicioDA.vb
+++ Centur/DataAccessLayer/BuscarServicioDA.vb
@@ -6,20 +6,22 @@ Public Class BuscarServicioDA
     Dim CmdText As String
 
 
-    Public Function BuscarServicio(ByVal nombre As String, ByVal categoria As String, ByVal zona As String)
-        Dim cmdtext As New System.Text.StringBuilder
-        cmdtext.AppendLine("SELECT * FROM Servicio")
-        cmdtext.AppendLine("WHERE nombre='" & nombre & "'")
-        cmdtext.AppendLine(" OR categoria='" & categoria & "'")
-        cmdtext.AppendLine(" OR zona='" & zona & "'")
-        Return SqlHelper.ExecuteDataset(_dbConnectionString, CommandType.Text, cmdtext.ToString())
+    Public Function BuscarServicio(ByVal nombre As String, ByVal categorias As String, ByVal zonas As String) As DataSet
+        Dim params() As SqlParameter
+        params = New SqlParameter() {New SqlParameter("@nombre", nombre), New SqlParameter("@categorias", categorias), New SqlParameter("@zonas", zonas)}
+
+        Return SqlHelper.ExecuteDataset(_dbConnectionString, CommandType.StoredProcedure, "BuscarServicio", params)
     End Function
 
     Public Function VerDetalleServicio(ByVal ServicioID As String) As DataSet
         Dim cmdtext As New System.Text.StringBuilder
         cmdtext.AppendLine("SELECT * FROM Servicio")
-        cmdtext.AppendLine("WHERE ID=" & ServicioID)
+        cmdtext.AppendLine("WHERE IdServicio=" & ServicioID)
         Return SqlHelper.ExecuteDataset(_dbConnectionString, CommandType.Text, cmdtext.ToString())
+    End Function
+
+    Function ArmarArbolCategorias() As DataSet
+        Throw New NotImplementedException
     End Function
 
 End Class

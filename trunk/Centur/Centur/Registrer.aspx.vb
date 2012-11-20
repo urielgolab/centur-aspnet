bb88cbdf438a37788446ccf2ce4ff685553840e7
maximiliano.mastrocola <maximiliano.mastrocola@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-11-20 01:01:03 +0000

    Compila

--- trunk/Centur/Centur/Registrer.aspx.vb
+++ trunk/Centur/Centur/Registrer.aspx.vb
@@ -6,18 +6,18 @@ Public Class Registrer
 
     Private Sub Registrarse_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Registrarse.Click
 
-        Dim rolUsuario As String = CStr(TipoUsuario.SelectedValue)
+        'Dim rolUsuario As String = CStr(TipoUsuario.SelectedValue)
 
-        Dim RegistroExitoso As Boolean = oLoginService.RegistrarUsuario(Me.NombreUsuario.Text, Me.Password.Text, Me.Telefono.Text, rolUsuario)
+        'Dim RegistroExitoso As Boolean = oLoginService.RegistrarUsuario(Me.NombreUsuario.Text, Me.Password.Text, Me.Telefono.Text, rolUsuario)
 
-        If RegistroExitoso Then
-            'Cargar todas las propiedades del usuario en variable de sesion / variable global.
-            'Mostrar mensaje de exito
-            Response.Redirect("~/Home.aspx")
-        Else
-            'Mostrar mensaje de error.
-            ErrorMessage.Text = "El Nombre de Usuario elegido está actualmente en uso."
-        End If
+        'If RegistroExitoso Then
+        '    'Cargar todas las propiedades del usuario en variable de sesion / variable global.
+        '    'Mostrar mensaje de exito
+        '    Response.Redirect("~/Home.aspx")
+        'Else
+        '    'Mostrar mensaje de error.
+        '    ErrorMessage.Text = "El Nombre de Usuario elegido está actualmente en uso."
+        'End If
 
 
     End Sub

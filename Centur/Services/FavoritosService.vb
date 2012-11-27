413e11664ca608ad8f4308280a3c1c2dbb2bb015
romanoguido87@gmail.com <romanoguido87@gmail.com@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-11-27 20:23:50 +0000

    Creacion de las clases favoritos en Services y DAL junto con los metodos en cada capa de alta de favorito y esFavorito
    sale uno?

---
+++ Centur/Services/FavoritosService.vb
@@ -0,0 +1,23 @@
+﻿Imports Entities
+
+Public Class FavoritosService
+
+    Dim oFavoritosDA As New DataAccessLayer.FavoritosDA
+
+
+    Public Function AltaFavoritos(ByVal idUsuario As Integer, ByVal idServicio As Integer, Optional ByRef Mensaje As String = "", Optional ByRef Status As Boolean = False) As Boolean
+        Return oFavoritosDA.AltaFavorito(idUsuario, idServicio, Mensaje, Status)
+    End Function
+
+    Public Function esFavorito(ByVal idUsuario As Integer, ByVal idServicio As Integer) As Boolean
+
+        Dim ds As DataSet = oFavoritosDA.esFavorito(idUsuario, idServicio)
+        If ds.Tables(0).Rows.Count > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+End Class

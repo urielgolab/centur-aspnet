a5d03604deb64db02d17bebccce2102b3e5c1e7e
romanoguido87@gmail.com <romanoguido87@gmail.com@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-11-27 20:23:50 +0000

    Creacion de las clases favoritos en Services y DAL junto con los metodos en cada capa de alta de favorito y esFavorito
    sale uno?

---
+++ trunk/Centur/DataAccessLayer/FavoritosDA.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.ApplicationBlocks.Data
+Imports System.Text
+
+Public Class FavoritosDA
+    Private _dbConnectionString As String = CType(Configuration.ConfigurationSettings.AppSettings("CenturConnStr"), String)
+
+    Public Function AltaFavorito(ByVal idUsuario As Integer, ByVal idServicio As Integer, Optional ByRef Mensaje As String = "", Optional ByRef Status As Boolean = False) As Boolean
+
+        Dim params() As SqlParameter
+        params = New SqlParameter() {New SqlParameter("@idUsuario", idUsuario), New SqlParameter("@idServicio", idServicio)}
+
+        SqlHelper.ExecuteDataset(_dbConnectionString, CommandType.StoredProcedure, "AltaFavorito", params)
+
+        Return True 'cambiar mas adelante
+
+    End Function
+
+    Public Function esFavorito(ByVal idUsuario As Integer, ByVal idServicio As Integer) As DataSet
+
+        Dim cmdtext As New StringBuilder
+        cmdtext.AppendLine("SELECT 1 FROM ServicioFavoritos")
+        cmdtext.AppendLine(" WHERE idUsuario=" & idUsuario & " and idServicio=" & idServicio)
+        Return SqlHelper.ExecuteDataset(_dbConnectionString, CommandType.Text, cmdtext.ToString())
+
+    End Function
+
+End Class

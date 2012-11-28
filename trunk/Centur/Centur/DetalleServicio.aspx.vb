d8e0a8b925eb9b3ae7bce1f3e69757401336ae1a
maximiliano.mastrocola@gmail.com <maximiliano.mastrocola@gmail.com@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-11-28 00:06:54 +0000

    Agregado BajaServicio de Guido

--- trunk/Centur/Centur/DetalleServicio.aspx.vb
+++ trunk/Centur/Centur/DetalleServicio.aspx.vb
@@ -5,13 +5,12 @@ Public Class DetalleServicio
     Dim oBuscarServicioService As New Services.BuscarServicioService()
     Dim oFavoritosService As New Services.FavoritosService()
     ' Dim usuarioID As Integer = CInt(Session("idUsuario"))
-    Dim ServicioID As Integer
+    Public Shared ServicioID As Integer
 
     Dim MinDate As Date = Date.Now.AddDays(-7)
     Dim MaxDate As Date = Date.Now.AddDays(7)
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        'If Not IsPostBack Then  --> AGREGAR EL POSTBACK, PERO VER COMO HACER PARA QUE SERVICIOID QUEDE LLENO!!
         'DivPedirTurno.Visible = False
         ServicioID = CInt(Request.QueryString("ServicioID"))
 
@@ -26,9 +25,8 @@ Public Class DetalleServicio
         If oFavoritosService.esFavorito(Login1.idUsuarioGlobal, ServicioID) Then
             Me.Favoritos.Text = "Quitar de Favoritos"
         Else
-            Me.Favoritos.Text = "Agregar a Favoritos"
+
         End If
-        'End If
 
     End Sub
 
@@ -45,11 +43,13 @@ Public Class DetalleServicio
         If Me.Favoritos.Text = "Agregar a Favoritos" Then
             If oFavoritosService.AltaFavoritos(Login1.idUsuarioGlobal, ServicioID) Then
                 Me.Mensaje.Text = "Agregado!!"
+                Me.Favoritos.Text = "Quitar de Favoritos"
             End If
         Else
-            'If oFavoritosService.BajaFavoritos(Login1.idUsuarioGlobal, ServicioID) Then
-            '    Me.Mensaje.Text = "Eliminado!!"
-            'End If
+            If oFavoritosService.BajaFavoritos(Login1.idUsuarioGlobal, ServicioID) Then
+                Me.Mensaje.Text = "Eliminado!!"
+                Me.Favoritos.Text = "Agregar a Favoritos"
+            End If
         End If
 
     End Sub

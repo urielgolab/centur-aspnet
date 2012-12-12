6f6c33d3229c720fb033de0eab937d1fedfcb116
romanoguido87@gmail.com <romanoguido87@gmail.com@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-12-12 20:18:08 +0000

    Avance en funcionalidad getGruposPropios para el proveedor

--- Centur/Centur/MisGrupos.aspx.vb
+++ Centur/Centur/MisGrupos.aspx.vb
@@ -2,4 +2,9 @@
 Partial Class MisGrupos
     Inherits System.Web.UI.Page
 
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        Label3.Text = DropDownList1.Text
+
+    End Sub
 End Class
--- Centur/Services/GruposService.vb
+++ Centur/Services/GruposService.vb
@@ -1,5 +1,25 @@
 ﻿Imports Entities
 
 Public Class GruposService
+    Dim oGruposDA As New DataAccessLayer.GruposDA
+
+    Public Function GetGruposPropios(ByVal idUsuario As Integer) As GrupoList
+
+        'Falta if para saber si es proveedor
+        Dim ds As DataSet = oGruposDA.GetGruposPropios(idUsuario)
+        Dim oGrupoList As New GrupoList
+
+        For Each dr As DataRow In ds.Tables(0).Rows
+            Dim oGrupo As New Grupo
+            oGrupo.ID = CInt(dr("IdGrupo"))
+            oGrupo.Tipo = CChar(dr("tipoGrupo"))
+            oGrupo.Nombre = CStr(dr("nombre"))
+            oGrupo.Descripcion = CStr(dr("descripcion"))
+            oGrupoList.Add(oGrupo)
+        Next
+
+        Return oGrupoList
+
+    End Function
 
 End Class

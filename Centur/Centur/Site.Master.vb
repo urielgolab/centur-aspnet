8393643a3d780a7e789a5001f09d4183a9871bb4
maximiliano.mastrocola@gmail.com <maximiliano.mastrocola@gmail.com@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-12-17 16:57:35 +0000

    Menu Dinamico segun rol de usuario, o sin estar logueado (cont)

--- Centur/Centur/Site.Master.vb
+++ Centur/Centur/Site.Master.vb
@@ -1,8 +1,37 @@
-﻿Public Class Site
+﻿Public Class Site1
     Inherits System.Web.UI.MasterPage
 
+    Dim oLoginService As New Services.LoginService()
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Session("Usuario") Is Nothing Then
+            Me.ArmarMenu(CType(Session("Usuario"), Entities.Usuario).TipoUsuario)
+        Else
+            Me.ArmarMenu("")
+        End If
 
     End Sub
+
+    Public Sub ArmarMenu(ByVal TipoUsuario As String)
+        If TipoUsuario = "" Then
+            'Antes de loguearse
+            NavigationMenu.Items.Add(New MenuItem("Login", "Login", "", "Login.aspx"))
+            NavigationMenu.Items.Add(New MenuItem("Registrarse", "Registrarse", "", "Registrer.aspx"))
+            NavigationMenu.Items.Add(New MenuItem("Buscar Servicio", "Buscar Servicio", "", "BuscarServicio.aspx"))
+        Else
+            'Proveedor y Cliente
+            NavigationMenu.Items.Add(New MenuItem("Inicio", "Inicio", "", "Home.aspx"))
+            NavigationMenu.Items.Add(New MenuItem("Buscar Servicio", "Buscar Servicio", "", "BuscarServicio.aspx"))
+            NavigationMenu.Items.Add(New MenuItem("Mis Grupos", "Mis Grupos", "", "MisGrupos.aspx"))
+            NavigationMenu.Items.Add(New MenuItem("Mis Turnos", "Mis Turnos", "", "MisTurnos.aspx"))
+            NavigationMenu.Items.Add(New MenuItem("Favoritos", "Favoritos", "", "Favoritos.aspx"))
+
+            If TipoUsuario = "P" Then
+                'Solo Proveedor
+                NavigationMenu.Items.Add(New MenuItem("Estadisticas", "Estadisticas", "", "VerEstadisticas.aspx"))
+            End If
+        End If
+    End Sub
+
 
 End Class

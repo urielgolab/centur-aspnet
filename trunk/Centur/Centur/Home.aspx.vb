f9c16de0d625d42cdab4100a595e1ed7325a09a4
maximiliano.mastrocola@gmail.com <maximiliano.mastrocola@gmail.com@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-12-17 16:53:25 +0000

    Menu Dinamico segun rol de usuario, o sin estar logueado

--- trunk/Centur/Centur/Home.aspx.vb
+++ trunk/Centur/Centur/Home.aspx.vb
@@ -5,6 +5,10 @@ Public Class Home
     Dim oLoginService As New Services.LoginService()
 
     Private Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Me.mensaje.InnerText = Login1.idUsuarioGlobal.ToString()
+        If Not Session("Usuario") Is Nothing Then
+            Dim user As Entities.Usuario = Session("Usuario")
+            Me.mensaje.InnerText = user.NombreUsuario + " | Tipo de usuario: " + user.TipoUsuario
+        End If
+
     End Sub
 End Class

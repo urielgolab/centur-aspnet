deadd5af13006c6fca90b97d64c54e16f27f3361
romanoguido87@gmail.com <romanoguido87@gmail.com@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2012-12-20 22:13:01 +0000

    primera version de metodos de modificacion y eliminacion de grupo

--- trunk/Centur/Centur/DetalleGrupo.aspx.vb
+++ trunk/Centur/Centur/DetalleGrupo.aspx.vb
@@ -4,10 +4,11 @@ Public Class DetalleGrupo
     Inherits System.Web.UI.Page
 
     Dim oGruposService As New Services.GruposService()
+    Dim oGrupo As New Grupo
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-        Dim oGrupo As New Grupo
+
         oGrupo = oGruposService.GetDetalleGrupo(CInt(Request.QueryString("id")))
         NombreGrupo.Text = oGrupo.Nombre
         DescripGrupo.Text = oGrupo.Descripcion
@@ -15,4 +16,17 @@ Public Class DetalleGrupo
 
     End Sub
 
+    Protected Sub LinkButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles LinkButton1.Click
+
+        oGrupo.ID = CInt(Request.QueryString("id"))
+        oGrupo.Nombre = NombreGrupo.Text
+        oGrupo.Descripcion = DescripGrupo.Text
+
+        oGruposService.UpdateGrupo(oGrupo)
+
+    End Sub
+
+    Protected Sub LinkButton2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles LinkButton2.Click
+        oGruposService.DeleteGrupo(CInt(Request.QueryString("id")))
+    End Sub
 End Class

f5e81f3ad869937465baec458db2fc433b0d3b6b
romanoguido87@gmail.com <romanoguido87@gmail.com@d6f9158e-6283-211e-6b7c-7445fb974b7c>
2013-01-04 00:54:17 +0000

    Primera version modificar grupo

---
+++ Centur/Centur/EditarGrupo.aspx.vb
@@ -0,0 +1,28 @@
+﻿Imports Entities
+
+Public Class EditarGrupo
+    Inherits System.Web.UI.Page
+    Dim oGruposService As New Services.GruposService()
+    Dim oGrupo As New Grupo
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim idGrupo As Integer = CInt(Request.QueryString("id"))
+
+        oGrupo = oGruposService.GetDetalleGrupo(idGrupo)
+        NombreGrupo.Text = oGrupo.Nombre
+        DescripGrupo.Text = oGrupo.Descripcion
+
+        If Not Page.IsPostBack Then
+            If oGrupo.MiembrosList.Count > 0 Then
+                GridMiembros.DataSource = oGrupo.MiembrosList
+                GridMiembros.DataBind()
+            Else
+                miembros.Visible = False
+                LabelNoMiembros.Visible = True
+            End If
+        End If
+
+    End Sub
+
+End Class
